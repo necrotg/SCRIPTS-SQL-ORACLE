1033b5fd24610cb656a0b43842d8ace56e300f58
Felipe Gabriel de Oliveira Milani <64813256+necrotg@users.noreply.github.com>
2020-12-09 08:37:16 -0300

    Add files via upload

---
+++ Script-13.sql
@@ -0,0 +1,11 @@
+GRANT ALL PRIVILEGES TO System;
+
+GRANT ALL PRIVILEGES TO crud;
+
+SELECT * FROM V$VERSION;
+
+mvn install:install-file -Dfile=D:\ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.2.0 -Dpackaging=jar
+
+DESC CRUD.FILMES;
+
+GRANT ALL PRIVILEGES TO trab;
